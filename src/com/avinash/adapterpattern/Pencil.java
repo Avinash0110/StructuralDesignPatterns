fcfd5fc92a86533c933f23ec0e54946f98b99c02
Avinash0110 <86357261+Avinash0110@users.noreply.github.com>
2021-09-21 12:01:56 +0530

    Delete src/com/avinash directory

--- src/com/avinash/adapterpattern/Pencil.java
+++
@@ -1,5 +0,0 @@
-package com.avinash.adapterpattern;
-
-public interface Pencil {
-    void draw(String str);
-}
--- src/com/avinash/lidpencil/LidPencil.java
+++
@@ -1,9 +0,0 @@
-package com.avinash.lidpencil;
-
-public class LidPencil
-{
-    public void mark(String str)
-    {
-        System.out.println(str);
-    }
-}
